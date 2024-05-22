406e321d8ead9932ab7461f47b7d5afea57a4769
Thomas Bonenfant <thomasjean.bonenfant@mail.polimi.it>
2024-05-22 17:02:47 +0200

    first commit

---
+++ compile.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+filetex="$1.tex"
+fileaux="$1.aux"
+
+# create pdf
+docker run --rm -it -v $(pwd):/workdir texlive/texlive pdflatex $filetex
+docker run --rm -it -v $(pwd):/workdir texlive/texlive bibtex $fileaux
+docker run --rm -it -v $(pwd):/workdir texlive/texlive pdflatex $filetex
+
+# remove useless files
+# ls -p -d * | grep -v "/$" | grep -E "(log|out|toc|lof|lot)$" | xargs rm
+
+# open pdf
+okular okular *.pdf
